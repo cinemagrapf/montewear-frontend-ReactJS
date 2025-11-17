bd599fb184dcd6bb16f5bb9d2de392e095a8eafd
cinemagrapf <neoplan1997@gmail.com>
2025-11-17 12:58:02 +0200

    Fixed categories and searc bar positioning

--- src/components/navigation/ProductCatalog.jsx
+++ src/components/navigation/ProductCatalog.jsx
@@ -1,20 +1,17 @@
 import ProductCategories from './product-page/ProductCategories.jsx';
 import ProductSearchBar from './product-page/ProductSearchBar.jsx';
 import ProductSorting from './product-page/ProductSorting.jsx';
+import './ProductCatalog.scss';
 
 const ProductCatalog = () => {
   return (
-    <div style={{ marginTop: '90px', paddingTop: '50px' }}>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}>
+    <div className="product-catalog-container">
+      <div className="top-bar">
         <ProductCategories />
         <ProductSearchBar />
       </div>
-      <div style={{ marginTop: '20px', textAlign: 'center' }}>
+
+      <div className="sorting-wrapper">
         <ProductSorting />
       </div>
     </div>
